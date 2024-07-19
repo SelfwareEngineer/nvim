8517f298538c228bb167ff63f9fe51bb2cad9a7c
SelfwareEngineer <126501922+SelfwareEngineer@users.noreply.github.com>
2024-07-19 09:02:49 -0400

    Add missing descriptions for tab navigation

--- lua/selfwareengineer/core/keymaps.lua
+++ lua/selfwareengineer/core/keymaps.lua
@@ -18,11 +18,11 @@ keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split windows equal size"
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close currently selected split" })
 
 -- Tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "" })
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 -- Yank to system clipboard
 keymap.set("n", "<leader>y", '"+y')
