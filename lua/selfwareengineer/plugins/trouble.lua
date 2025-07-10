25759a690f95b0ae933c8e3c4829cf901c04a3ee
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-10 14:36:57 -0400

    Update trouble.nvim config to reflect new version

--- lua/selfwareengineer/plugins/trouble.lua
+++ lua/selfwareengineer/plugins/trouble.lua
@@ -4,14 +4,14 @@ return {
 	cmd = "Trouble",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
 	keys = {
-		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open/close trouble diagnostics" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
 		{
-			"<leader>xX",
+			"<leader>xx",
 			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-			desc = "Open/close trouble diagnostics (current buffer only)",
+			desc = "Buffer Diagnostics (Trouble)",
 		},
-		-- { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-		-- { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-		-- { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix List (Trouble)" },
+		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Location List (Trouble)" },
+		-- { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" }, (leftover from old Trouble, not sure if this is still useful or even possible)
 	},
 }
