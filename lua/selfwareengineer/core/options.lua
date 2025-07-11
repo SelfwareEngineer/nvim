f75d38b264526b907e1a22e432fd741ae87aa7b1
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-10 21:45:34 -0400

    vim.opt teaks
    - scrolloff: keeps line buffer while scrolling for better visibility
    - incsearch: incremental search; updates highlighting with each
    keystroke
    - undodir + undofile: allow undo through more than just current session
    - termguicolors: me like good color

--- lua/selfwareengineer/core/options.lua
+++ lua/selfwareengineer/core/options.lua
@@ -1,9 +1,11 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-local opt = vim.opt
+local opt = vim.opt -- for conciseness
 
+-- navigation
 opt.relativenumber = true -- shows relative numbers to the cursor line to make vim movements easier
 opt.number = true
+opt.scrolloff = 8
 
 -- tabs and indentation
 opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
@@ -14,6 +16,7 @@ opt.autoindent = true -- copy indent from current line when starting new one
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.incsearch = true -- search highlighting updates with each keystroke
 
 opt.cursorline = true -- draws line to make cursor easier to find
 
@@ -23,3 +26,10 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split windows
 opt.splitright = true -- split vertical windows to the right
 opt.splitbelow = true -- split horizontal windows to the bottom
+
+-- Allow undotree to access more than just the current session
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+-- Colors good. Me like.
+opt.termguicolors = true
