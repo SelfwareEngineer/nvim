2ede714468847a39f3057259f1ca39bfd5b6faff
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-12 01:05:20 -0400

    fix which-key.lua per :checkhealth
    - explicitly add nvim-web-devicons and mini.nvim as dependencies

--- lua/selfwareengineer/plugins/which-key.lua
+++ lua/selfwareengineer/plugins/which-key.lua
@@ -1,11 +1,12 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function ()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- add opts here or leave empty to use default settings
-  }
+	"folke/which-key.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.nvim" },
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		-- add opts here or leave empty to use default settings
+	},
 }
