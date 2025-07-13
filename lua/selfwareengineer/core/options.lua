cb893621f5dcd5d468e0252cd0ef8ca54519d580
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-13 13:18:09 -0400

    Tweaks to core/options.lua
    - Add softtabstop, smartindent, inccommand, background (dark)
    - Disable wrapping to build better habits
    - Add smart backspace behavior
    - Improve several comments

--- lua/selfwareengineer/core/options.lua
+++ lua/selfwareengineer/core/options.lua
@@ -8,10 +8,16 @@ opt.number = true
 opt.scrolloff = 8
 
 -- tabs and indentation
-opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+opt.tabstop = 4 -- 4 spaces for a tab
+opt.softtabstop = 4 -- 4 spaces for tab key
 opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true -- smart indenting on new lines
+opt.wrap = false -- don't wrap lines (if your lines are wrapping, they're probably too long)
+
+-- backspace behavior
+opt.backspace = { "indent", "eol", "start" } -- allow backspacing over indent, end of line, and start of insert
 
 -- undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]]) -- undercurl for diagnostics
@@ -21,13 +27,14 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]]) -- reset undercurl
 -- opt.spell = true
 -- opt.spelllang = { "en_us" }
 --
--- show diagnostics messages
+-- show diagnostics messages - disable if using lsp_lines.nvim to avoid duplication
 -- vim.diagnostic.config({ virtual_text = true })
 
 -- search settings
+opt.incsearch = true -- search highlighting updates with each keystroke
+opt.inccommand = "split" -- show search results in a split window
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.incsearch = true -- search highlighting updates with each keystroke
 
 opt.cursorline = true -- draws line to make cursor easier to find
 
@@ -38,9 +45,10 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical windows to the right
 opt.splitbelow = true -- split horizontal windows to the bottom
 
--- Allow undotree to access more than just the current session
+-- Allow undo history (including undotree) to access more than just the current session
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
 -- Colors good. Me like.
 opt.termguicolors = true
+opt.background = "dark"
