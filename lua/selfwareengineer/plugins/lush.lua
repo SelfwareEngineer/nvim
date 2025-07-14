94655c1cd59ed05528816e432cb3e25563655f3d
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-14 00:15:50 -0400

    BROKEN: First pass at lush.nvim config. See details.
    - Lazy.nvim isn't recognizing the path to the config as a plugin like
    the lush.nvim documentation claims it should. Need to come back to this
    later with a fresh brain and see if I can debug. For now, just going to
    eyescorch/lua/lush-theme/eyescorch.lua and running :Lushify works as a
    temp fix.

--- lua/selfwareengineer/plugins/lush.lua
+++ lua/selfwareengineer/plugins/lush.lua
@@ -2,8 +2,5 @@ return {
 	"rktjmp/lush.nvim",
 	-- if you wish to use your own colorscheme:
 
-	-- { dir = "~/.config/nvim/lua/selfwareengineer/lush-themes/eyescorch", lazy = true },
-	-- config = function ()
-	--     vim.cmd("colorscheme eyescorch")
-	-- end
+	{ dir = "home/selfwareengineer/.config/nvim/lua/selfwareengineer/lush-themes/eyescorch", lazy = true },
 }
